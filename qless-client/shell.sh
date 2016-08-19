67cbf09eb8c77a81ada4757edcf3408602fb0dc6
Faisal Khan <faisal.nust@gmail.com>
2016-08-19 13:34:56 -0500

    Updated script to run multitau jobs.

--- qless-client/shell.sh
+++ qless-client/shell.sh
@@ -2,10 +2,13 @@
 
 #!/bin/bash
 
-echo `pwd`
-interval=$1
+# echo `pwd`
+# interval=$1
 
-for i in `seq 1 $interval`; do
-    echo [ $$ ]  [`date +"%T"`] "Sleeping for 10 seconds"
-    sleep 10s
-done
+# for i in `seq 1 $interval`; do
+#     echo [ $$ ]  [`date +"%T"`] "Sleeping for 10 seconds"
+#     sleep 10s
+# done
+
+cd XPCS_HADOOP_DIR
+./multitau.sh -i $1
--- qless-client/submit.py
+++ qless-client/submit.py
@@ -2,9 +2,10 @@ import qless
 import xpcs
 
 client = qless.Client()
-job1_id = client.queues['multitau'].put(xpcs.XPCSAnalysis, {'arg':1})
-job2_id = client.queues['gridftp'].put(xpcs.XPCSAnalysis, {'arg': 2}, depends=[job1_id])
-job3_id = client.queues['plot'].put(xpcs.XPCSAnalysis, {'arg': 3}, depends=[job1_id, job2_id])
+job1_id = client.queues['multitau'].put(xpcs.XPCSAnalysis, {'i':})
+
+#job2_id = client.queues['gridftp'].put(xpcs.XPCSAnalysis, {'arg': 2}, depends=[job1_id])
+#job3_id = client.queues['plot'].put(xpcs.XPCSAnalysis, {'arg': 3}, depends=[job1_id, job2_id])
 
 
 # for i in range(1):
